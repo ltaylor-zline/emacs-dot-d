53b2b6d06395cf12f0932cdd43cb588602b92ad5
Luke Taylor <luke@lptsoftware.com>
2022-01-16 21:27:50 -0800

    Refactored font setter in font.el

--- ui/gui/font.el
+++ ui/gui/font.el
@@ -2,15 +2,15 @@
   "Checks if a font is available"
   (find-font (font-spec :name font-name)))
 
-(defun set-font-if-exists (font-name callback)
+(defun set-font-if-exists (font-name)
   "Sets a font if it exists"
-  (if
-    (font-avail font-name)
-    (set-frame-font font-name)
-    (callback)))
+  (if (font-avail font-name)
+      (set-frame-font font-name)))
 
 ;; Try to set font to UB Mono, then try Noto Sans Mono
 (set-font-if-exists
- "Ubuntu Mono 18"
- (set-font-if-exists
-  "Noto Sans Mono" ()))
+ "Ubuntu Mono 18")
+(set-font-if-exists
+ "Noto Sans Mono")
+(set-font-if-exists
+ "Hack NF-18.0")
